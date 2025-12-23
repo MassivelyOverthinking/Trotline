1f17942b794e8e8b70b1a5edb8e68b80c796eeee
Simon Stensgaard <STcena1@hotmail.com>
2025-12-23 23:32:28 +0100

    Added docstring to 'from_response_to_model' method

--- src/backend/application/utility/utility_methods.py
+++ src/backend/application/utility/utility_methods.py
@@ -11,12 +11,35 @@ from botocore.response import StreamingBody
 # ----------------------------------------
 
 def from_reponse_to_model(body: StreamingBody) -> xgb.Booster:
+    """
+    Extract the necessary binary data from S3 (AWS) client session, and build internal new XGboost
+    inference model for URL-string predictions.
+
+    Parameters
+    ----------
+    body : StreamingBody
+        AWS client response.
+
+    Returns
+    -------
+    xgboost.Booster
+        XGBoost inference model for URL-string predictions.
+
+    Exceptions
+    ----------
+    ModuleNotFoundError
+        Raised if the StreamingBody element contains no binary data for XGBoost modeling. 
+    """
+
+    # Extract the data from the 'Body' element.
     model_body = body["Body"]
     model_bytes = model_body.read()
 
+    # Raise appropriate error if no data was found.
     if not model_bytes:
         raise ModuleNotFoundError("XGBoost Model file not loaded correctly!")
     
+    # Train & return the finalized inference model.
     xgb_model = xgb.Booster()
     xgb_model.load_model(bytearray(model_bytes))
 
