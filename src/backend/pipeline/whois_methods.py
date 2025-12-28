a04c956c465f2113eb7b873ae760846a23169618
Simon Stensgaard <STcena1@hotmail.com>
2025-12-28 22:50:45 +0100

    Added docstring to 'retrieve_extracted_url_data' method

--- src/backend/pipeline/whois_methods.py
+++ src/backend/pipeline/whois_methods.py
@@ -8,7 +8,7 @@ import datetime
 
 from tldextract import ExtractResult
 from datetime import timezone
-from typing import Optional, Union, Dict
+from typing import Optional, Union, Dict, Tuple
 from whois.exceptions import WhoisError, WhoisCommandFailedError, WhoisDomainNotFoundError
 
 from src.backend.pipeline.helper_methods import extract_hostname, is_domain_typosquatted
@@ -20,8 +20,23 @@ from src.backend.pipeline.dataset import typosquatted_domains_set
 
 # Extract the necessary WHOIS documentation regarding the specified URL domain.
 # Currently returns the days since Registration & days until Expiration.
-def get_whois_info(text: ExtractResult):
+def get_whois_info(text: ExtractResult) -> Tuple[int | None, int | None]:
+  """
+  Check the parsed URL-result information against the WHOIS registry - online URL database for
+  storing essential demographic data regarding websites -, and extract the necessary information.
+  Utility method in Trotline finalised Data Pipeline.
+  
+  Parameters
+  ----------
+  text : tldextract.ExtractResult
+    The tldextract-based Result-class containing URL-related information and data.
 
+  Returns
+  -------
+  tuple[str | None, str | None]
+    Returns two distinct, optional integer values representing the days since WHOIS registration
+    and days until WHOIS expiration.
+  """
   # Retrieve the full URL from ExtractResult.
   text = f"{text.subdomain}.{text.domain}.{text.suffix}"
 
@@ -49,7 +64,21 @@ def get_whois_info(text: ExtractResult):
   
 # Wrapper method for extracting, processing and formulating WHOIS Data correctly.
 # Returns a Python Dict.
-def retrieve_extracted_url_data(url: str) -> Dict[str, Optional[Union[float, int]]]:
+def retrieve_extracted_url_data(url: str) -> Dict[str, Union[float | None, int | None]]:
+  """
+  Method for extracting the specified URL's WHOIS registry data and information - 
+  Utility method in Trotline finalised Data Pipeline.
+  
+  Parameters
+  ----------
+  url : str
+    Suspected URL-string to check against the WHOIS registry.
+
+  Returns
+  -------
+  dict[str, union[float | None, int | None]]
+    Dictionary containing WHOIS-related and typosquatting data. 
+  """
   # Parse the URL to retreive critical information.
   extracted_url = extract_hostname(url=url)
 
